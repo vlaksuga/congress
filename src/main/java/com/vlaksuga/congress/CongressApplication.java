f7f0e8ca8162be6039ad588dadeeaa15270b79c8
vlaksuga <45654321+vlaksuga@users.noreply.github.com>
2021-06-03 14:40:39 +0900

    Init Project
    Init congress project

---
+++ src/main/java/com/vlaksuga/congress/CongressApplication.java
@@ -0,0 +1,13 @@
+package com.vlaksuga.congress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CongressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlaksuga/congress/CongressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlaksuga.congress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
